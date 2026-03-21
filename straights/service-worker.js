f96fc6b5a39ab2de865ff35338c9bea715d5d104
m-ringler <42344272+m-ringler@users.noreply.github.com>
2026-03-21 22:06:20 +0100

    Straights.Web v0.8.5

--- straights/service-worker.js
+++ straights/service-worker.js
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: 2025-2026 Moritz Ringler
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
-const CACHE_NAME = 'v0.8.3';
+const CACHE_NAME = 'v0.8.5';
 const urlsToCache = [
     './',
     './.htaccess',
